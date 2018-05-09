97fc1deddaed798b65ad100b7da1cdc66d53cadd
maxim-h <maksim.holmatov@gmail.com>
2018-05-09 20:23:11 +0300

    Node implemented (badly) Made a retarded implementation of Node for my binary tree with method ::new for creating a node from either a character and its frequency or from two child nodes.

--- haffman_coding_4-2-5/src/binary_tree.rs
+++ haffman_coding_4-2-5/src/binary_tree.rs
@@ -1,49 +1,68 @@
+use std::boxed::Box;
 /// Simple binary tree implementation
 ///TODO: implement traits:
 ///                       .remove
 ///
-
-
-
-
-struct Node {
-    freq: u32,
+///
+#[derive(Debug)]
+pub struct Node {
     ch: Option<char>,
+    freq: u32,
     l_0: Option<Box<Node>>,
     r_1: Option<Box<Node>>,
 }
 
 
+
 impl Node {
-    fn new(f: Option<u32>, c: Option<char>, l: Option<Box<Node>>, r: Option<Box<Node>> ) -> Node {
-        Node {
-              freq: match f {
-                  Some(f) => f,
-                  None(_) => l.freq + r.freq,
-              },
-              ch: match c {
-                  Some(c) => c,
-                  None(_) => None,
-              },
-              l_0: match l {
-                  Some(l) => l,
-                  None(_) => None,
-              },
-              r_1: match r {
-                  Some(r) => r,
-                  None(_) => None,
-              },
-             }
+
+    fn get_freq<'a>(&'a self) -> &'a u32 {
+        &self.freq
     }
 
+    pub fn new(c: Option<char>, f: Option<u32>, l: Option<Box<Node>>, r: Option<Box<Node>> ) -> Box<Node> {
+        let mut leaf = false;
+        let mut left= Box::new(Node {
+            ch: None,
+            freq: 0,
+            l_0: None,
+            r_1: None,
+        });
+        let mut right= Box::new(Node {
+            ch: None,
+            freq: 0,
+            l_0: None,
+            r_1: None,
+        });
+
+        match l {
+            Some(l) => {left = l;},
+            None => {leaf  = true;},
+        };
+
+        match r {
+            Some(r) => {right = r;},
+            None => {leaf = true;},
+        };
+
+        if !leaf {
+            let sum_freq = right.get_freq() + left.get_freq();
+
+            return Box::new(Node {
+                        ch: None,
+                        freq: sum_freq,
+                        l_0: Option::from(left),
+                        r_1: Option::from(right),
+                        })
+        } else {
+            return Box::new(Node {
+                        ch: c,
+                        freq: f.unwrap(),
+                        l_0: None,
+                        r_1: None,
+                        })
+        }
+    }
 }
-/*
-
-struct BinaryTree {
-    nodes: Vec<Node>
-}
 
 
-
-
-*/
--- haffman_coding_4-2-5/src/main.rs
+++ haffman_coding_4-2-5/src/main.rs
@@ -1,29 +1,29 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
 pub mod character_queue;
+pub mod binary_tree;
 
 use std::io::stdin;
 //use std::collections::HashMap;
 use character_queue::PriorityQueue;
 use character_queue::FromString;
-
+use binary_tree::Node;
 
 
 fn main() {
+    /* TEST PriorityQueue
     let mut s: String = String::new();
     stdin().read_line(&mut s).expect("Didn't read, lol");
     let s: String = s.trim().parse().expect("Couldn't parse, lol");
 
     let mut q: PriorityQueue<char> = PriorityQueue::from_string(&s);
 
-
-    /*
-    queue.insert(1, 12);
-    queue.insert(2, 11);
-    queue.insert(3, 10);
-    queue.insert(4, 9);
-    queue.insert(5, 8);
-    queue.insert(6, 7);
-
-    println!("{:?}", queue.extract_min());
-    */
     println!("{:?}", q.extract_min());
+    */
+
+    let n1= Node::new(Option::from('f'), Option::from(3), Option::from(None), Option::from(None));
+    let n2= Node::new(Option::from('c'), Option::from(5), Option::from(None), Option::from(None));
+    let n3= Node::new(Option::from(None), Option::from(None), Some(n1), Some(n2));
+    print!("{:?}", n3)
 }
